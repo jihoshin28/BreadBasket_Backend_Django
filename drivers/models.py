fe08a882bed8907da59c8262f28125a3802d431b
geeuho <geeuho@gmail.com>
2020-06-06 08:24:25 -0700

    removed files

--- drivers/models.py
+++ drivers/models.py
@@ -9,6 +9,7 @@ class Driver(models.Model):
     state = models.CharField(max_length = 20)
     phone = models.BigIntegerField()
     email = models.EmailField(max_length = 100)
-
+    
     def __str__(self):
         return self.name
+
